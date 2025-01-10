f6e06f3141c3389dd6d63e44df824188ced45c9f
Jayesh_Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-01-10 10:41:13 +0530

    Deleted Unwanted Files

--- MostSender.py
+++
@@ -1,9 +0,0 @@
-f=open("mbox-short.txt")
-count=0
-d={}
-final_d={}
-for line in f:
-    if line.startswith("From "):
-            d[(line.split(" ")[1])]=d.get((line.split(" ")[1]),0)+1
-final_d=dict(sorted(d.items(),key=lambda x: x[1],reverse=True))
-print(final_d)
--- readwriteopreations.py
+++
@@ -1,7 +0,0 @@
-f=open("writefile.txt","r+")
-print(f.read())
-f.seek(4)
-f.write("xyz")
-f.seek(0)
-print(f.read())
-f.close()
